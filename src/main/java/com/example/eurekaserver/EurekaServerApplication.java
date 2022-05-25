8ab71db9147a71a0cdf8b861871088f02d1be93d
junyeong <gowjr207@gmail.com>
2022-05-25 14:03:11 +0900

    docs: 주석

--- src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * access url
+ * UI Dashboard -> localhost:8761
+ * Micro Service List -> localhost:8761/eureka/apps
+ */
 
 @EnableEurekaServer
 @SpringBootApplication
